9cb14f410e417fc51436f939c468cb60811653f6
Hebert Romero <hjr@iqthink.com>
2017-07-18 15:16:46 -0500

    ajustes payment logs user

--- src/app/settings/payment/payment.component.ts
+++ src/app/settings/payment/payment.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireDatabase, FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2/database';
 import { DatauserService } from '../../providers/datauser.service';
+import { LogsUserService } from '../../providers/logs-user.service';
+import * as moment from 'moment/moment';
 
 @Component({
   selector: 'app-payment',
   templateUrl: './payment.component.html',
-  styleUrls: ['./payment.component.css']
+  styleUrls: ['./payment.component.css'],
+  providers : [DatauserService, LogsUserService ]
 })
 export class PaymentComponent implements OnInit {
 
@@ -13,8 +16,12 @@ export class PaymentComponent implements OnInit {
   private  payment: FirebaseListObservable<any[]>;
   private  restaurant : any;
   private  paymentData : any;
+  private  test : FirebaseListObservable<any[]>;
+  private  jobData : any;
+  private  dateNow;
+  private  dataLogUser : any;
 
-  constructor(private af : AngularFireDatabase, private du : DatauserService) { }
+  constructor(private af : AngularFireDatabase, private du : DatauserService,  private logsUser : LogsUserService) { }
 
   ngOnInit() {
 
@@ -25,7 +32,8 @@ export class PaymentComponent implements OnInit {
 
         data.map( data => {
           this.restaurant = {
-              restaurant : data.restaurant
+              restaurant : data.restaurant,
+              key : data.$key
           }
           this.payment = this.af.list('/paymentFrecuency', {
             query:{
@@ -35,6 +43,7 @@ export class PaymentComponent implements OnInit {
           });
         });
       });
+      console.log(this.restaurant);
   }
 
 
@@ -56,6 +65,8 @@ export class PaymentComponent implements OnInit {
 
           }
         );
+
+        this.logUserAction(this.restaurant.key, this.restaurant.restaurant, 'Create', 'Payment');
 
   }
 
@@ -86,8 +97,29 @@ export class PaymentComponent implements OnInit {
   {
 
     this.payment.update(this.key, data);
+    this.logUserAction(this.restaurant.key, this.restaurant.restaurant, 'Update', 'Payment');
   }
 
+
+  deletePayment( data : any)
+  {
+     console.log(data);
+     this.payment.remove(data);
+     this.logUserAction(this.restaurant.key, this.restaurant.restaurant, 'Delete', 'Payment');
+  }
+
+
+  logUserAction(userKey : any, restaurant : any, action_user: any , from_action:any){
+
+    this.dataLogUser = {
+
+      id_currentUser :userKey,
+      mane_user : restaurant,
+      action_user : action_user,
+      from_action :from_action,
+    };
+    this.logsUser.createLogUser(this.dataLogUser);
+  }
 
 
 }
